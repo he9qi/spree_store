dc1e317be37e43aa85b3e578373e9fbd6f52fcf0
Qi He <qihe229@gmail.com>
2011-07-16 00:03:08 -0700

    first commit spree template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpreeStore::Application.config.secret_token = '8702ff1393d693b832944dd81d5e4a38f49168dd93c82bd063a7f76772af041f303e402a08b4486f5854000b09fcac715176f730c7bfeb4a9a1b8842685e3846'
